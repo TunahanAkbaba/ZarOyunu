322eb855653aeea970839eb33958afe018e54735
TunahanAkbaba <hilmi.akbaba@gmail.com>
2020-01-11 16:32:12 +0300

    ilk gonderme

---
+++ src/fonksiyon/Fonksiyon.java
@@ -0,0 +1,19 @@
+package fonksiyon;
+
+
+public class Fonksiyon {
+    public static void main(String[] args) {
+            
+            int x = (int) (Math.random()*5+1);
+             
+            if(x==1) System.out.println("\n *\n\n");
+            if(x==2) System.out.println("*\n\n  *\n");
+            if(x==3) System.out.println("*\n *\n  *\n");
+            if(x==4) System.out.println("* *\n * \n* *\n");
+            if(x==5) System.out.println("* * *\n* * *\n* * *\n");
+
+            }
+        }   
+    
+    
+
